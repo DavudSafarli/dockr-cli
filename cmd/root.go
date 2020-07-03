b8099675c9fffa6ae01699c9b2d481be0d9c5934
Davud Safarli <davudseferli@gmail.com>
2020-07-03 21:07:42 +0400

    add fast print func

--- cmd/root.go
+++ cmd/root.go
@@ -1,6 +1,11 @@
 package cmd
 
 import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"os"
+
 	"github.com/spf13/cobra"
 )
 
@@ -14,4 +19,14 @@ func Execute() error {
 	}
 
 	return rootCmd.Execute()
+}
+
+func encode(v interface{}) string {
+	buf := new(bytes.Buffer)
+	json.NewEncoder(buf).Encode(v)
+	return buf.String()
+}
+
+func print(v interface{}) {
+	fmt.Fprint(os.Stdout, encode(v))
 }
