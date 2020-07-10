ebcc0dd6e656ececf859613c86b47fe947cfbe74
Davud Safarli <davudseferli@gmail.com>
2020-07-10 17:29:46 +0400

    listen only container events

--- cmd/events.go
+++ cmd/events.go
@@ -2,7 +2,7 @@ package cmd
 
 import (
 	"context"
-
+	"github.com/docker/docker/api/types/events"
 	"github.com/docker/docker/api/types/filters"
 	"github.com/spf13/cobra"
 
@@ -13,17 +13,21 @@ import (
 func ListenEvents() *cobra.Command {
 	return &cobra.Command{
 		Use:   "listen",
-		Short: "returns all containers as array in json",
+		Short: "long-running process to listen docker events",
 		RunE: func(cmd *cobra.Command, args []string) error {
 			cli := GetDockerClient()
+			filters := filters.NewArgs()
+			filters.Add("type", events.ContainerEventType)
+
 			eventChan, _ := cli.Events(context.Background(), types.EventsOptions{
 				Since:   "",
 				Until:   "",
-				Filters: filters.Args{},
+				Filters: filters,
 			})
 			for {
 				select {
 				case event := <-eventChan:
+					//time.Sleep(time.Millisecond * 100)
 					print(encode(event))
 				}
 			}
